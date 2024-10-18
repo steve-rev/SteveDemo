6b74a471c4691a94a022db1c368a6ca89de1a0d8
Steve <steve@Steves-MacBook-Pro.local>
2024-10-18 10:01:41 -0400

    changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# EKS-Terraform-Jenkins
 EKS-Terraform-Jenkins
+
+"GitOps" procedure for provisioning EKS in private subnets, with a Load balancer service.  
+
+To Do - provision MongoDB (Terraform done for this), an S3 bucket
+
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 locals {
   region = "us-east-1"
-  name   = "stevewizdemo"
+  name   = "stevedemo"
   vpc_cidr = "10.123.0.0/16"
   azs      = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
