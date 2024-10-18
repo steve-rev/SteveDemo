98140c5688954280e296d195cc94736acaf91115
Steve <steve@Steves-MacBook-Pro.local>
2024-10-18 08:47:03 -0400

    smaller worker nodes

--- terraform/eks/eks.tf
+++ terraform/eks/eks.tf
@@ -1,48 +1,61 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
+  version = "~> 20.0"
 
-  cluster_name                   = local.name
-  cluster_endpoint_public_access = true
+  cluster_name                             = var.cluster_name
+  cluster_version                          = var.cluster_version
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
 
   cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
     vpc-cni = {
-      most_recent = true
+      before_compute = true
+      most_recent    = true
+      configuration_values = jsonencode({
+        env = {
+          ENABLE_POD_ENI                    = "true"
+          ENABLE_PREFIX_DELEGATION          = "true"
+          POD_SECURITY_GROUP_ENFORCING_MODE = "standard"
+        }
+        nodeAgent = {
+          enablePolicyEventLogs = "true"
+        }
+        enableNetworkPolicy = "true"
+      })
     }
   }
 
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
-
-    attach_cluster_primary_security_group = true
-  }
+  create_cluster_security_group = false
+  create_node_security_group    = false
 
   eks_managed_node_groups = {
-    amc-cluster-wg = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+    default = {
+      instance_types           = ["t3.small"]
+      force_update_version     = true
+      release_version          = var.ami_release_version
+      use_name_prefix          = false
+      iam_role_name            = "${var.cluster_name}-ng-default"
+      iam_role_use_name_prefix = false
 
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
+      min_size     = 3
+      max_size     = 6
+      desired_size = 3
 
-      tags = {
-        ExtraTag = "hellowiz"
+      update_config = {
+        max_unavailable_percentage = 50
+      }
+
+      labels = {
+        workshop-default = "yes"
       }
     }
   }
 
-  tags = local.tags
+  tags = merge(local.tags, {
+    "karpenter.sh/discovery" = var.cluster_name
+  })
 }
+
